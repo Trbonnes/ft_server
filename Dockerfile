fa9da996bf9d393c8c0b12ebd0cd1e575194bd0b
Tristan BONNESON <trbonnes@e2r3p14.42.fr>
2019-12-16 13:44:35 +0100

    config

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,16 @@
 FROM debian:buster
 MAINTAINER Bonneson Tristan <trbonnes@student.42.fr>
 
-#COPY src/nginx.conf /ect/ngninx/nginx.conf
-COPY src/default.conf /etc/nginx/sites-available/
 COPY src/wordpress.zip /var/www/html/
+COPY src/phpMyAdmin.zip /var/www/html/
 COPY src/start.sh .
+
 EXPOSE 80
+
 RUN bash start.sh
+
 CMD service nginx start && \
 	service mysql start && \
 	service php7.3-fpm start && \
-	tail -f /dev/null
+	bash
+	#tail -f /dev/null
--- src/start.sh
+++ src/start.sh
@@ -6,7 +6,7 @@
 #    By: trbonnes <trbonnes@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/12/16 09:15:11 by trbonnes          #+#    #+#              #
-#    Updated: 2019/12/16 11:59:31 by trbonnes         ###   ########.fr        #
+#    Updated: 2019/12/16 13:28:14 by trbonnes         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,21 +14,16 @@ apt-get update
 apt-get install -y wget
 apt-get install -y nginx
 apt-get install -y mariadb-server
-apt-get install -y php
-apt-get install -y php-cli php-fpm php-cgi
+#apt-get install -y php
+apt-get install -y php-fpm
 apt-get install -y php-mysql
-apt-get install -y php-mbstring
-apt-get install -y openssl
 apt-get install -y zip
 
 
 unzip /var/www/html/wordpress.zip -d /var/www/html/
-
+unzip /var/www/html/phpMyAdmin.zip -d /var/www/html/
 
 service mysql start
 mysql -u root -e "CREATE DATABASE wordpress_db"
 mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress_db.* TO 'root'@'localhost'"
-
-ln -s /etc/nginx/sites-available/default.conf /etc/nginx/sites-enabled/
-chown -R www-data:www-data /var/www/html/*
-chmod -R 755 /var/www/html/*
+mysql -u root -e "FLUSH PRIVILEGES"
