6a801bbb606306a5eec54b39b5bac77130cd92be
Tristan BONNESON <trbonnes@e2r3p14.42.fr>
2019-12-17 10:20:19 +0100

    end

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,11 @@ MAINTAINER Bonneson Tristan <trbonnes@student.42.fr>
 
 COPY src/wordpress.zip /var/www/html/
 COPY src/phpMyAdmin.zip /var/www/html/
-COPY src/localhost.conf /etc/nginx/sites-available/default
+COPY src/localhost.conf /etc/nginx/sites-available/localhost
 COPY src/wp-config.php /var/www/html/wordpress/wp-config.php
+COPY src/domain.crt /etc/ssl/certs/localhost.crt
+COPY src/domain.key /etc/ssl/private/localhost_key.key
 COPY src/start.sh .
-
-EXPOSE 80
 
 RUN bash start.sh
 
--- src/start.sh
+++ src/start.sh
@@ -6,7 +6,7 @@
 #    By: trbonnes <trbonnes@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/12/16 09:15:11 by trbonnes          #+#    #+#              #
-#    Updated: 2019/12/16 14:53:15 by trbonnes         ###   ########.fr        #
+#    Updated: 2019/12/17 09:30:43 by trbonnes         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,7 +14,6 @@ apt-get update
 apt-get install -y wget
 apt-get install -y nginx
 apt-get install -y mariadb-server
-#apt-get install -y php
 apt-get install -y php-fpm
 apt-get install -y php-mysql
 apt-get install -y zip
@@ -26,7 +25,7 @@ rm /var/www/html/wordpress.zip
 rm /var/www/html/phpMyAdmin.zip
 mv /var/www/html/phpMyAdmin-4.9.2-all-languages var/www/html/phpMyAdmin  
 
-ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
+ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/
 
 service mysql start
 mysql -u root -e "CREATE DATABASE wordpress_db"
